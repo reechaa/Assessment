8272b752de2dbce00b9eede068c2ee222d3a461d
Richa Dhakal <126368853+reechaa@users.noreply.github.com>
2025-03-21 15:52:15 +0545

    Create array.java

---
+++ array.java
@@ -0,0 +1,18 @@
+public class InversionCount{
+public static int countInversion(int[] array){
+int count=0;
+for(int i=0;i<array.length;i++){
+for(int j=i+1;j<array.length;j++){
+if(array[i]>array[j]){
+count++;
+}
+}
+}
+return count;
+}
+public static void main(string[] args){
+int[] array={2,4,8,9,3};
+int totalInversion=countInversion(array);
+system.out.println("Inversion:"+totalInversion);
+}
+}
